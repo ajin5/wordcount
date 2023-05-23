f41fc3861907c151994ced2c1ac7a7574e0b9a4b
ajin5 <ajinmla94@gmail.com>
2023-05-23 14:51:24 +0530

    joly

--- counterapp/views.py
+++ counterapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+@csrf_exempt
 def counter(request):
     wordcounter1=request.POST.get("text")
     amount=len((wordcounter1).replace(',','').split())
